a4731c97f592307bcd865e45ae4973869da27c76
Aman kohare <amankohare@gmail.com>
2024-09-17 21:27:10 +0530

    add basic

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 
 // Use the user router with a base route
+app.get("/" , (req , res)=>{
+    res.send("helo this is aman")
+})
 app.use('/api', router);
 
 // Connect to MongoDB
