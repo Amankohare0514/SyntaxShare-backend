59afb600b0bb39de4839dcb141de4eb0f74aa30f
Aman kohare <amankohare@gmail.com>
2024-09-18 16:41:39 +0530

    add url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://syntax-share-frontend.vercel.app/'
   }));
   
 app.use(express.json());
