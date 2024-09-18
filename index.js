ff7f6152c76d3268122a08480e97b1eb532ed2e4
Aman kohare <amankohare@gmail.com>
2024-09-18 09:52:58 +0530

    everything working fine

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = require("./routes/user.router");
 const mongoose = require("mongoose");
 const app = express();
-// Middleware to parse JSON request body
+
 app.use(express.json());
 
-// Use the user router with a base route
+
 app.get("/" , (req , res)=>{
     res.send("helo this is aman")
 })
@@ -15,13 +15,17 @@ app.get('/ping', (req, res) => {
 });
 app.use('/api', router);
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://amankohare:amankohare1234@cluster0.jw6km.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {})
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('Connection error:', err));
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => {
+    console.error('MongoDB connection error:', err);
+    res.status(500).json({ error: 'Database connection error' });
+});
 
 
-// Start the server
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
